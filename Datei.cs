0f466699e1013f6c000480025b59bbed6c9c974f
JuergenMarek <151005924+JuergenMarek@users.noreply.github.com>
2024-04-22 10:39:46 +0200

    V2

---
+++ Datei.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+/// <summary>
+/// Klasse Dateioperationen mit den zugehörigen Methoden
+/// </summary>
+class Datei
+{
+    /// <summary>
+    /// Methode für einzelne Datei öffnen
+    /// </summary>
+    public static void DateiOeffnenUndEinlesen()
+    {
+        try
+        {
+            // Dialog Datei öffnen anzeigen
+            OpenFileDialog dialogDateiOeffnen = new OpenFileDialog();
+            // Filter auf *.txt setzen
+            dialogDateiOeffnen.Filter = "Textdateien (*.txt)|*.txt";
+            // Titel des Fensters
+            dialogDateiOeffnen.Title = "Bitte eine Datei auswählen";
+
+            // Abfrage, ob Öffnen der Datei erfolgreich war
+            if (dialogDateiOeffnen.ShowDialog() == DialogResult.OK)
+            {
+                // Setzen der Variable für den Dateinamen
+                string dateiname = dialogDateiOeffnen.FileName;
+                string dateiinhalt = "";
+
+                // Einlesen des Dateiinhalts und Ausgabe am Bildschirm
+                using (StreamReader einlesen = new StreamReader(dateiname))
+                {
+                    Console.WriteLine("Dateiinhalt:");
+                    dateiinhalt = einlesen.ReadLine();
+                    Console.WriteLine(dateiinhalt);
+                }
+
+                // Übergeben des Dateinamens an die nächste Methode
+                AuswahlBearbeiten(dateiname, dateiinhalt);
+            }
+        }
+        // Exceptionhandling
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fehler beim Öffnen der Datei: {ex.Message}");
+            Console.Write("Drücken Sie eine Taste, um zum Hauptmenü zurückzukehren.");
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// Methode für Auswahl Bearbeiten j/n
+    /// </summary>
+    /// <param name="dateiname">Ausgewählte Datei</param>
+    /// <param name="dateiinhalt">Inhalt der ausgewählten Datei</param>
+    private static void AuswahlBearbeiten(string dateiname, string dateiinhalt) {
+        // Abfrage für Bearbeitung der Datei
+        Console.Write("Möchten Sie die Datei bearbeiten? (j/n): ");
+        string auswahlBearbeiten = Console.ReadLine();
+
+        switch (auswahlBearbeiten.ToLower())
+        {
+            case "j":
+                AuswahlHinzufuegenOderLoeschen(dateiname, dateiinhalt);
+                break;
+
+            case "n":
+                break;
+
+            default:
+                Console.WriteLine("Ungültige Eingabe. Bitte geben Sie nur j oder n ein.");
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Auswahl ob Text hinzugefügt oder gelöscht werden soll
+    /// </summary>
+    /// <param name="dateiname">Ausgewählte Datei</param>
+    /// <param name="dateiinhalt">Inhalt der ausgewählten Datei</param>
+    private static void AuswahlHinzufuegenOderLoeschen(string dateiname, string dateiinhalt)
+        {
+        // Abfrage welche Operation mit dem Text
+        Console.WriteLine("Wollen Sie:");
+        Console.WriteLine("1___Text hinzufügen");
+        Console.WriteLine("2___Text entfernen");
+        Console.Write("Bitte wählen Sie eine Option: ");
+        string auswahlTextoperation = Console.ReadLine();
+
+        // Auswahl abhandeln
+        switch (auswahlTextoperation)
+        {
+            case "1":
+                // Einlesen des neuen Texts
+                Console.WriteLine();
+                Console.Write("Bitte geben Sie den hinzuzufügenden Text ein: ");
+                string neuerText = Console.ReadLine();
+
+                //Hinzufügen des neuen Texts zum bestehenden Text
+                dateiinhalt = dateiinhalt + " " + neuerText;
+                DateiSpeichern(dateiname, dateiinhalt);
+                break;
+
+            case "2":
+                // Eingabe für zu löschenden Text
+                Console.WriteLine();
+                Console.Write("Bitte geben Sie den zu löschenden Text ein: ");
+                string textZuLoeschen = Console.ReadLine();
+
+                // Ersetzen des zu löschenden Text mit ""
+                dateiinhalt = dateiinhalt.Replace(textZuLoeschen, "");
+                DateiSpeichern(dateiname, dateiinhalt);
+                break;
+
+            default:
+                Console.WriteLine("Ungültige Eingabe. Die Datei wurde nicht bearbeitet.");
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Methode für das Speichern des modifizierten Texts
+    /// </summary>
+    /// <param name="dateiname">Ausgewählte Datei</param>
+    /// <param name="dateiinhalt">Inhalt der ausgewählten Datei</param>
+    private static void DateiSpeichern(string dateiname, string dateiinhalt)
+        {
+        // Abfrage, ob gesamter Text in gleiche oder neue Datei geschrieben werden soll
+        Console.WriteLine();
+        Console.WriteLine("Möchten Sie den Text:");
+        Console.WriteLine("1___In die gleiche Datei speichern");
+        Console.WriteLine("2___In eine neue Datei speichern");
+        Console.Write("Bitte Auswahl eingeben: ");
+        string auswahlTextSpeichern = Console.ReadLine();
+
+        switch (auswahlTextSpeichern)
+        {
+            // Dateiinhalt n gleiche Datei speichern
+            case "1":
+                File.WriteAllText(dateiname, dateiinhalt);
+                Console.WriteLine("Datei gespeichert.");
+                Console.Write("Drücken Sie eine Taste, um zum Hauptmenü zurückzukehren.");
+                Console.ReadKey();
+                break;
+
+            // In neue Datei speichern
+            case "2":
+                // Fenster für Datei speichern öffnen
+                SaveFileDialog dialogDateiSpeichern = new SaveFileDialog();
+                dialogDateiSpeichern.Filter = "Textdateien (*.txt)|*.txt";
+                dialogDateiSpeichern.Title = "Datei speichern unter";
+
+                if (dialogDateiSpeichern.ShowDialog() == DialogResult.OK)
+                {
+                    // Speichern des Dateiinhalts in die neue Datei
+                    File.WriteAllText(dialogDateiSpeichern.FileName, dateiinhalt);
+                    Console.WriteLine("Datei gespeichert.");
+                    Console.Write("Drücken Sie eine Taste, um zum Hauptmenü zurückzukehren.");
+                    Console.ReadKey();
+                }
+                // Fehlermeldung, wenn Datei nicht gespeichert werden konnte
+                else
+                {
+                    Console.WriteLine("Fehler. Die Datei wurde nicht gespeichert.");
+                    Console.Write("Drücken Sie eine Taste, um zum Hauptmenü zurückzukehren.");
+                    Console.ReadKey();
+                }
+                break;
+
+            default:
+                Console.WriteLine("Ungültige Eingabe. Die Datei wurde nicht bearbeitet.");
+                break;
+        }
+    } 
+}
---
+++ Menu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+/// <summary>
+/// Hauptmenü
+/// </summary>
+class Mainmenu
+{
+    public static void Menu()
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            try
+            {
+                // Hauptmenü generieren
+                Console.Clear();
+                Console.WriteLine("Herzlich willkommen im Texteditor der Firma DocuEdit GmbH!");
+                Console.WriteLine();
+                Console.WriteLine("Hauptmenü:");
+                Console.WriteLine("1___Einzelne Datei öffnen");
+                Console.WriteLine("2___Ganzen Ordner öffnen");
+                Console.WriteLine("3___Programmende");
+
+                Console.Write("Bitte wählen Sie eine Option: ");
+                string auswahl = Console.ReadLine();
+
+                // Auswahl behandeln
+                switch (auswahl)
+                {
+                    case "1":
+                        Datei.DateiOeffnenUndEinlesen();
+                        break;
+                    case "2":
+                        Ordner.OrdnerOeffnen();
+                        break;
+                    case "3":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Ungültige Eingabe. Bitte wählen Sie eine Option zwischen 1 und 3.");
+                        Console.Write("Drücken Sie eine Taste, um zum Hauptmenü zurückzukehren.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Es ist ein Fehler aufgetreten: {ex.Message}");
+            }
+        }
+    }
+
+
+}
